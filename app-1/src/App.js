443a34f7a1ac4539306f9e96c23e1b3820398fad
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2021-09-24 19:48:46 -0600

    app1

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState}from 'react';
 import './App.css';
 
+
+
+
+
 function App() {
+
+  const [data, setData] = useState()
+
+  function getData (e) {
+    setData(e.target.value)
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          App #1
-        </a>
-      </header>
+     <h1>Type Here</h1>
+     <input type="text" onChange={getData} />
+     <p>
+       {data}
+     </p>
     </div>
   );
 }
